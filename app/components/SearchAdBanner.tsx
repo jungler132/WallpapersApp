dde985d8cc96a7158813da04d7fe3f04889e110e
jungler132 <jungler145@gmail.com>
2025-05-01 23:16:27 +0400

    banner back

--- app/components/SearchAdBanner.tsx
+++ app/components/SearchAdBanner.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-// import { BannerAd, BannerAdSize } from 'react-native-google-mobile-ads';
+import { BannerAd, BannerAdSize } from 'react-native-google-mobile-ads';
 
 export const SearchAdBanner = () => {
   return (
     <View style={styles.container}>
-      {/* <BannerAd
+      <BannerAd
         unitId="ca-app-pub-3940256099942544/6300978111"
         size={BannerAdSize.BANNER}
         onAdLoaded={() => console.log('Search ad loaded')}
         onAdFailedToLoad={(error) => console.error('Search ad failed to load:', error)}
-      /> */}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    width: '100%',
     alignItems: 'center',
     marginVertical: 8,
   },
